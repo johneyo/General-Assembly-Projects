108b1d340d08b6d82ff116ca7d289d6c28f48f7c
johney <johneyo57@yahoo.com>
2019-02-25 14:46:54 +0100

    finally done

--- Build your own personal website/index.js
+++ Build your own personal website/index.js
@@ -4,14 +4,16 @@ const btn = document.getElementById('btn');
 btn.addEventListener('click', onclick)
 
 
-function onclick (e) {
-if(email.value){
-btn.style.background ='#28a745',
-btn.value = 'Done'
-}else{
-    btn.style.background ='#dc3545'
-    btn.value = 'no email'
 
-}
+function onclick (e) {
+    const re = /^([A-Za-z0-9_\-\.]+)@([A-Za-z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if(!re.test(email.value)){
+    btn.style.background ='#dc3545'
+        btn.value = 'no email'
+  }else{
+    btn.style.background ='#28a745',
+    btn.value = 'Done'  }  
+
     e.preventDefault();
 }
